e3064d0151c914803f3e05fef76e668a79ad52e1
7coil <leondrolio@gmail.com>
2019-02-26 14:43:02 +0000

    make more incompatible with internet explorer 11

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,13 +1,8 @@
-self.addEventListener('install', function(e) {
-	self.skipWaiting();
-  });
+self.addEventListener('install', () => self.skipWaiting());
   
-  self.addEventListener('activate', function(e) {
-	self.registration.unregister()
-	  .then(function() {
-		return self.clients.matchAll();
-	  })
-	  .then(function(clients) {
-		clients.forEach(client => client.navigate(client.url))
-	  });
-  });
+self.addEventListener('activate', () => {
+  self.registration
+	  .unregister()
+    .then(() => self.clients.matchAll())
+    .then(clients => clients.forEach(client => client.navigate(client.url)));
+});
