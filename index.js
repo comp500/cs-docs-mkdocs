2d47df8c8ffe692ee89b6c29be6248f12e93d609
comp500 <comp500@users.noreply.github.com>
2019-05-12 14:36:22 +0100

    Remove KaTeX on pages that don't use it + preload

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const AsciiMathParser = require("esm")(module)("asciimath2tex");
 const parser = new AsciiMathParser.default();
 const katex = require("katex");
 const path = require("path");
-const entities = new require('html-entities').AllHtmlEntities;
+const entities = new require("html-entities").AllHtmlEntities;
+const minify = require("html-minifier").minify;
 
 let walker = walk.walk("./site");
+
+const katexSrc = /{% block extrahead %}\s*([\s\S]+?)\s*{% endblock %}/
+	.exec(fs.readFileSync("./theme/main.html", "utf8"))[1]
+	.replace(/\r\n/g, "\n");
 
 walker.on("file", function(root, fileStats, next) {
 	if (!fileStats.name.endsWith(".html")) {
@@ -18,13 +23,24 @@ walker.on("file", function(root, fileStats, next) {
 			next();
 			return;
 		}
+		let replaced = false;
 		data = data.replace(/`([^`]+?)`/g, (math, mathTrimmed) => {
-			console.log(entities.decode(mathTrimmed));
+			replaced = true;
+			//console.log(entities.decode(mathTrimmed));
 			const tex = parser.parse(entities.decode(mathTrimmed));
-			console.log(tex);
+			//console.log(tex);
 			return katex.renderToString(tex, {
 				throwOnError: true
 			});
+		});
+		if (!replaced) {
+			// Remove KaTeX CSS
+			data = data.replace(katexSrc, "");
+		}
+		data = minify(data, {
+			collapseWhitespace: true,
+			preserveLineBreaks: true,
+			removeComments: true
 		});
 		console.log(path.join(root, fileStats.name));
 		fs.writeFileSync(path.join(root, fileStats.name), data);
